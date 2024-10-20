cf70c546749fa7adaa97041210a9b97b65494894
Mert Batuhan ÜNVERD <mert.batuhan.unverdi@saritay.com>
2024-10-20 21:56:39 +0300

    modified env.py on alembic

--- alembic/env.py
+++ alembic/env.py
@@ -13,8 +13,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Add your model's MetaData object here
-target_metadata = db.Model.metadata  # Modelin MetaData nesnesini ayarlayın
+target_metadata = db.Model.metadata  
 
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
